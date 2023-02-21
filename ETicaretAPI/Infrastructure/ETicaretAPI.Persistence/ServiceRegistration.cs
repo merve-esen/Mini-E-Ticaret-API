095a6479b6751e10cefdc9b99f42954479039c97
Merve Esen <merveesen834@gmail.com>
2023-02-21 14:53:32 +0300

    Migration and configuration added

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -8,7 +8,7 @@ namespace ETicaretAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=ETicaretAPIDb;"));
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
         }
     }
 }
