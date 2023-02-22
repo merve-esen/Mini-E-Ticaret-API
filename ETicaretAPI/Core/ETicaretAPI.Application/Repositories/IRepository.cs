65cddc5b2397fa57231ad404a4e611b5c349761e
Merve Esen <merveesen834@gmail.com>
2023-02-22 16:18:51 +0300

    Generic Repository Design Pattern

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        DbSet<T> Table { get; }
+    }
+}
---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ETicaretAPI.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class
+    {
+        Task<bool> AddAsync(T model);
+        Task<bool> AddRangeAsync(List<T> datas);
+        bool Remove(T model);
+        bool RemoveRange(List<T> datas);
+        Task<bool> RemoveAsync(string id);
+        bool Update(T model);
+
+        Task<int> SaveAsync();
+    }
+}
