38d250e6a05208a7622dc41b311c4d660041d50c
Merve Esen <merveesen834@gmail.com>
2023-03-21 16:55:01 +0300

    feat: add file upload

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/ServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ETicaretAPI.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection collection)
+        {
+            collection.AddHttpClient();
+        }
+    }
+}
