3f9a6a03b93f81a1728841766ec532de9b061b25
Merve Esen <merveesen834@gmail.com>
2023-03-22 11:48:35 +0300

    feat: add file upload

--- ETicaretAPI/Core/ETicaretAPI.Domain/Entities/File.cs
+++ ETicaretAPI/Core/ETicaretAPI.Domain/Entities/File.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ETicaretAPI.Domain.Entities
 {
-    internal class File
+    public class File : BaseEntity
     {
+        public string FileName { get; set; }
+        public string Path { get; set; }
+        public string Storage { get; set; }
+        [NotMapped]
+        public override DateTime UpdatedDate { get => base.UpdatedDate; set => base.UpdatedDate = value; }
     }
 }
--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -13,6 +13,7 @@ namespace ETicaretAPI.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Domain.Entities.File> Files { get; set; }
 
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
